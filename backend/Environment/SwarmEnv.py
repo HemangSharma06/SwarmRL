87e84f9444ef9df96ee790be42086396d69f9d8f
HemangSharma06 <146320981+HemangSharma06@users.noreply.github.com>
2026-08-20 21:15:49 +0530

    Changed the environment according to the drones

--- backend/Environment/SwarmEnv.py
+++ backend/Environment/SwarmEnv.py
@@ -1,25 +1,29 @@
 import numpy as np
-import gymnasium as gym
 from gymnasium import spaces
 from pettingzoo import ParallelEnv
 
+from backend.Environment.drone import Drone
+
 
 class SwarmEnv(ParallelEnv):
-    metadata = {"name": "swarm_env_v0"}
-    
+
+    metadata = {
+        "name": "swarm_env_v0"
+    }
+
     def __init__(self, num_agents=2, space_size=100):
         super().__init__()
-
-        self.num_agents = num_agents
         self.space_size = space_size
 
         # Agent names
         self.possible_agents = [
-            f"drone_{i}" for i in range(num_agents)
+            f"drone_{i}"
+            for i in range(num_agents)
         ]
 
         self.agents = self.possible_agents.copy()
 
+        # Action space: [velocity_x, velocity_y, velocity_z]
         self.action_spaces = {
             agent: spaces.Box(
                 low=-1.0,
@@ -30,6 +34,7 @@ class SwarmEnv(ParallelEnv):
             for agent in self.possible_agents
         }
 
+        # Observation space: [x, y, z]
         self.observation_spaces = {
             agent: spaces.Box(
                 low=0.0,
@@ -39,35 +44,40 @@ class SwarmEnv(ParallelEnv):
             )
             for agent in self.possible_agents
         }
+        # Drone objects
+        self.drones = {}
 
-        self.positions = {}
+    def observation_space(self, agent):
+        return self.observation_spaces[agent]
+
+    def action_space(self, agent):
+        return self.action_spaces[agent]
 
     def reset(self, seed=None, options=None):
-
         if seed is not None:
             np.random.seed(seed)
 
+        # Reset active agents
         self.agents = self.possible_agents.copy()
 
-        self.positions = {
-            agent: np.random.uniform(
-                0,
-                self.space_size,
-                size=3
-            ).astype(np.float32)
+        # Creating drones
+        self.drones = {
+            agent: Drone(
+                drone_id=agent,
+                space_size=self.space_size
+            )
             for agent in self.agents
         }
 
+        # Initial observations
         observations = {
-            agent: self.positions[agent].copy()
+            agent: self.drones[agent].get_position()
             for agent in self.agents
         }
-
         infos = {
             agent: {}
             for agent in self.agents
         }
-
         return observations, infos
 
     def step(self, actions):
@@ -77,26 +87,22 @@ class SwarmEnv(ParallelEnv):
         truncations = {}
         infos = {}
 
+        # Moving the each drone
         for agent, action in actions.items():
-            action = np.asarray(action, dtype=np.float32)
-
-            self.positions[agent] += action
-            self.positions[agent] = np.clip(
-                self.positions[agent],
-                0,
-                self.space_size
+            action = np.asarray(
+                action,
+                dtype=np.float32
             )
+            self.drones[agent].move(action)
+
         for agent in self.agents:
-
             rewards[agent] = 0.0
-
             terminations[agent] = False
             truncations[agent] = False
-
             infos[agent] = {}
 
         observations = {
-            agent: self.positions[agent].copy()
+            agent: self.drones[agent].get_position()
             for agent in self.agents
         }
 
